64eabd8a7231a20d029114fde78e9245553ebb64
unknown <kwin11bvb@yandex.by>
2021-04-08 21:55:39 +0300

    Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from "./Header/Header";
 
-function App() {
-  return (
-    <div>
-      portfolio
-    </div>
-  );
+const App = (
+    {}
+) => {
+    return (
+        <Header />
+    );
 }
 
 export default App;
---
+++ src/Header/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import s from './Header.module.css';
+import Nav from "./Nav/Nav";
+
+const Header = (
+    {}
+) => {
+    return (
+        <div className={s.header}>
+            <Nav />
+        </div>
+    );
+}
+
+export default Header;
