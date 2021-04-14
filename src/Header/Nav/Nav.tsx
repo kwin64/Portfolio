e66fb0bc79e1edecba58e36f5a1fc081fc3edd6d
kwin64 <kwin11bvb@yandex.by>
2021-04-14 17:30:15 +0300

    header navBar fix hover::after menu

--- src/Header/Nav/Nav.tsx
+++ src/Header/Nav/Nav.tsx
@@ -13,7 +13,10 @@ const Nav: React.FC<PropsNavType> = (
     }
 ) => {
     const navbar = stateForHeader.map(s => {
-        return <a href='#'><img src={s.image} title={s.title}/></a>
+        return <a href='#' data-title={s.title}>
+            <img src={s.image} title={s.title}/>
+            <span>{s.title}</span>
+        </a>
     })
     return (
         <div className={s.nav}>
